0dc861be510a69be84df55b43d45e8f7b864139e
Ephraimraxy <124668425+Ephraimraxy@users.noreply.github.com>
2026-02-13 13:45:21 +0100

    Add express.static to serve frontend assets

--- server/index.js
+++ server/index.js
@@ -41,6 +41,9 @@ app.get('/api/db-test', async (req, res) => {
     }
 });
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../dist')));
+
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get(/.*/, (req, res) => {
